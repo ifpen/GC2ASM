f185a4d4ffd7fbda6febd9c479ea979d13d33061
Kévin BELELLOU <kevin.belellou.externe@ifpen.fr>
2024-11-13 16:13:16 +0100

    Close .ch file after reading it, avoiding some leaks

--- src/main/java/fr/ifpen/allotropeconverters/gc/chemstation/chfile/ChFileFactory.java
+++ src/main/java/fr/ifpen/allotropeconverters/gc/chemstation/chfile/ChFileFactory.java
@@ -10,16 +10,17 @@ public class ChFileFactory {
 
     public ChFile getChFile(String filePath) throws IOException {
         File file = new File(filePath);
-        RandomAccessFile input = new RandomAccessFile(file,"r");
-        String version = readString(input, false);
+        try (RandomAccessFile input = new RandomAccessFile(file, "r")) {
+            String version = readString(input, false);
 
-        switch (version){
-            case "179":
-                return new ChFile179(input);
-            case "181":
-                return new ChFile181(input);
-            default:
-                throw new IOException("version not supported");
+            switch (version) {
+                case "179":
+                    return new ChFile179(input);
+                case "181":
+                    return new ChFile181(input);
+                default:
+                    throw new IOException("version not supported");
+            }
         }
     }
 }
